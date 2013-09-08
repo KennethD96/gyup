9618f58ab83f9a53f6fbe5d101cf17009c10059a
KennethD96 <Kenneth.dahle.1996@gmail.com>
2013-09-08 16:00:11 +0200

    initial commit

---
+++ gyup.php
@@ -0,0 +1,51 @@
+<?php
+	// Gyazo Upload Script for K96.co
+	// error_reporting(E_ERROR); // Prevents PHP spewing out debug info to the client.
+	// find -type f -name '*.png' | while read f; do mv "$f" "${f%.png}"; done // This command will be used to remove the .png extention from old files.
+		
+	// Variables
+		
+		$echoURL = 'http://beta.K96.co/';
+		$savePath = '/var/www/beta/i/';
+		
+		$randChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		$fileExist = false;
+		
+		include('../inc/SQL_auth.php'); // $SQL_server, $SQL_db, $SQL_user, $SQL_password
+		
+	// Body
+
+		function save($idLen) {
+			global $echoURL, $savePath, $randChars, $fileExist, $imgID, $imgPath;
+			
+			for ( $c = 0; $c <= 32; $c++) {	
+				$imgID = substr(str_shuffle(str_repeat($randChars, $idLen)), 0 , $idLen);
+				$imgPath = $savePath . $imgID;
+				
+				if (file_exists($imgPath)) {
+					$fileExist = true;
+				
+				} else {
+					$fileExist = false;
+					move_uploaded_file($_FILES['imagedata']['tmp_name'], $imgPath);
+					echo $echoURL . $imgID;
+						SQL($imgID);
+					break;
+		}}}
+		
+		function SQL($imgID) {
+			global $SQL_server, $SQL_user, $SQL_password, $SQL_db;
+			$date = date("Y-m-d H:i:s");
+			$sql_connect = new mysqli($SQL_server, $SQL_user, $SQL_password, $SQL_db);
+			mysqli_query($sql_connect, "INSERT INTO image ( ID, DATE, VIEWS ) VALUES ('" . $imgID ."', '" . $date . "', 0 )" );
+				mysqli_close($sql_connect);
+		}
+		
+	if(isset($_FILES['imagedata']['tmp_name'])) {
+		save(5);
+			if ($fileExist == true) {
+				save(6);
+					if ($fileExist == true) {
+					echo 'error: Could not generate image ID. Please contact administrator.'; 
+	}}}		else { include("huehue.php"); echo $hue; } // Insert your own Easter Egg here.
+?>

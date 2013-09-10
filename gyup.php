82b46dbe9f827b3b9db456d5c062863868749893
Kenneth Dahle <Kenneth.dahle.1996@gmail.com>
2013-09-10 14:52:23 +0200

    Implementing user management

--- gyup.php
+++ gyup.php
@@ -2,16 +2,16 @@
 	// Gyazo Upload-script for K96.co
 	// error_reporting(E_ERROR); // Prevents PHP spewing out debug info to the client.
 	// find -type f -name '*.png' | while read f; do mv "$f" "${f%.png}"; done // This command will be used to remove the .png extention from old files.
-		
+
 	// Variables
-		
+
 	$echoURL = 'http://beta.K96.co/';
 	$savePath = '/var/www/beta/i/';
 	$validUagent = 'GyazoKD/0.30';
 	$randChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	
+
 	$SQL_enabled = true;
-		
+
 	// Body
 
 	include('../inc/SQL_auth.php'); // $SQL_server, $SQL_db, $SQL_user, $SQL_password
@@ -21,14 +21,14 @@
 
 		function save($idLen) {
 			global $validUagent, $echoURL, $savePath, $randChars, $fileExist, $imgID, $imgPath;
-			
+
 			for ( $c = 0; $c <= 32; $c++) {
 				$imgID = substr(str_shuffle(str_repeat($randChars, $idLen)), 0 , $idLen);
 				$imgPath = $savePath . $imgID;
-				
+
 				if (file_exists($imgPath)) {
 					$fileExist = true;
-				
+
 				} else {
 					$fileExist = false;
 					move_uploaded_file($_FILES['imagedata']['tmp_name'], $imgPath);
@@ -39,18 +39,20 @@
 					SQL($imgID);
 					break;
 		}}}
-		
+
 		function SQL($imgID) {
 			global $imgFormat, $SQL_enabled, $SQL_server, $SQL_user, $SQL_password, $SQL_db;
 			if ($SQL_enabled == true) {
 				$date = date("Y-m-d H:i:s");
 				$sql_connect = new mysqli($SQL_server, $SQL_user, $SQL_password, $SQL_db);
 					mysqli_query($sql_connect, "INSERT INTO image ( ID, DATE, VIEWS, FORMAT, ADDR ) VALUES ('" . $imgID ."', '" . $date . "', 0, '" . $imgFormat . "', '" . $_SERVER['REMOTE_ADDR'] . "')" );
+					if($_SERVER['ID'])) {
+					mysqli_query($sql_connect, "UPDATE user SET IMAGES = IMAGES + '" . $imgID . "' WHERE ID = '" . $_SERVER['ID']. "'"); }
 					mysqli_close($sql_connect);
 		}}
-	
+
 	//Start
-	
+
 	if(isset($_FILES['imagedata']['tmp_name'])) {
 		if($imgFormat >= 1) { // Checks if the uploaded file is a valid image.
 		if($imgSize >= 16) { // Checks that image contains at least 16 pixels.

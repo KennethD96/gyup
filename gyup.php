bf6dc39582ad37b673a95b73b04c2d94263eff21
Kenneth Dahle <Kenneth.dahle.1996@gmail.com>
2013-09-11 09:54:16 +0200

    fixes and cleanup

--- gyup.php
+++ gyup.php
@@ -1,6 +1,6 @@
 <?php
 	// Gyazo Upload-script for K96.co
-	// error_reporting(E_ERROR); // Prevents PHP spewing out debug info to the client.
+	// error_reporting(E_ALL); // Prevents PHP spewing out debug info to the client.
 	// find -type f -name '*.png' | while read f; do mv "$f" "${f%.png}"; done // This command will be used to remove the .png extention from old files.
 
 	// Variables
@@ -18,18 +18,20 @@
 	$imgSize = $imgWidth + $imgHeight;
 	$date = date("Y-m-d H:i:s");
 	$fileExist = false;
+	$newID = false;
 	
 	if (isset($_FILES['id']) == false) {
-		$cID = hash('md5', $_SERVER['REMOTE_ADDR'] . date("YmdHis"));
+		$UID = hash('md5', $_SERVER['REMOTE_ADDR'] . date("YmdHis"));
+		$newID = true;
 	} else {
-		$cID = $_FILES['id']; }
-	header('X-Gyazo-Id' . $cID);
+		$UID = $_FILES['id']; }
+	header('X-Gyazo-Id' . $UID);
 
 		function save($idLen) {
 			global $validUagent, $echoURL, $savePath, $randChars, $fileExist, $imgID, $imgPath;
 
 			for ( $c = 0; $c <= 32; $c++) {
-				$imgID = (str_shuffle(str_repeat($randChars, $idLen)), 0 , $idLen);
+				$imgID = substr(str_shuffle(str_repeat($randChars, $idLen)), 0 , $idLen);
 				$imgPath = $savePath . $imgID;
 
 				if (file_exists($imgPath)) {
@@ -38,7 +40,7 @@
 				} else {
 					$fileExist = false;
 					move_uploaded_file($_FILES['imagedata']['tmp_name'], $imgPath);
-					if($_SERVER['HTTP_USER_AGENT'] == $validUagent) {
+					if ($_SERVER['HTTP_USER_AGENT'] == $validUagent) {
 						echo $echoURL . $imgID;
 					} else {
 						echo $echoURL . 'dl/?file=gyazo&id=' . $imgID; }
@@ -47,23 +49,23 @@
 		}}}
 
 		function SQL($imgID) {
-			global $cID, $imgFormat, $date, $SQL_enabled, $SQL_server, $SQL_user, $SQL_password, $SQL_db;
-			if ($SQL_enabled == true) {
+			global $newID, $UID, $imgFormat, $date, $SQL_enabled, $SQL_server, $SQL_user, $SQL_password, $SQL_db;
+			if ($SQL_enabled) {
 				$sql_connect = new mysqli($SQL_server, $SQL_user, $SQL_password, $SQL_db);
+					if ($newID) { mysqli_query($sql_connect, "INSERT INTO users ( UID ) VALUES ('" . $UID . "') "); }
 					mysqli_query($sql_connect, "INSERT INTO images ( ID, DATE, VIEWS, FORMAT ) VALUES ('" . $imgID ."', '" . $date . "', 0, '" . $imgFormat . "')" );
-					mysqli_query($sql_connect, "UPDATE users SET IMAGES = CONCAT('" . $imgID . ", ', IMAGES) WHERE UID = '" . $cID . "')";
+					mysqli_query($sql_connect, "UPDATE users SET IMAGES = CONCAT('" . $imgID . ", ', IMAGES) WHERE UID = '" . $UID . "')");
 					mysqli_close($sql_connect);
 		}}
 
 	//Start
 
 	if(isset($_FILES['imagedata']['tmp_name'])) {
-		if($imgFormat >= 1) { // Checks if the uploaded file is a valid image.
-		if($imgSize >= 16) { // Checks that image contains at least 16 pixels.
+		if ($imgFormat >= 1) { // Checks if the uploaded file is a valid image format.
+		if ($imgSize >= 16) { // Checks that image contains at least 16 pixels.
 			save(5);
-				if ($fileExist == true) {
+				if ($fileExist) {
 					save(6);
-					if ($fileExist == true) {
+					if ($fileExist) {
 						echo 'error: Could not generate image ID. Please contact administrator.';
 	}}}}} else { include("huehue.php");} // Insert your own Easter Egg here.
-?>
